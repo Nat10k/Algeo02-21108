865323e380d0266335783dc500db2c2c8c950958
Nat10k <110515021+Nat10k@users.noreply.github.com>
2022-10-30 13:03:50 +0700

    Dataset sebagian, kode bikin norm face

--- cobaOpenCV.py
+++ cobaOpenCV.py
@@ -1,54 +1,111 @@
+from statistics import covariance
 import cv2
 import numpy as np
+import os
+import splitfolders
 import glob
 
-# Akses folder training dataset
-# folder_dir = 'D:\OneDrive - Institut Teknologi Bandung\Folder Kuliah\Sem 3\Aljabar Linier dan Geometri\Tubes\Tubes 2\lfw-funneled\Angelina_Jolie'
+def transpose(mtrx) :
+    # Menghasilkan transpose matris mtrx
+    # KAMUS LOKAL
+    # TMtrx : array of array of int
+    # TArr : array of int
+    # i,j : integer
+
+    # ALGORITMA
+    TMtrx = []
+    for i in range (len(mtrx[0])) :
+        TArr = []
+        for j in range (len(mtrx)) :
+            TArr.append(mtrx[j][i])
+        TMtrx.append(TArr)
+    return TMtrx
+
+def multiplyMatrix(mtrx1,mtrx2) :
+    # Mengalikan mtrx1 dan mtrx2
+    # KAMUS LOKAL
+    # mResult : array of array of int
+    # i,j : integer
+    # mulResult : integer
+
+    # ALGORITMA
+    mResult = [[0 for j in range(len(mtrx2[0]))] for i in range(len(mtrx1))]
+    for i in range(len(mResult)) :
+        for j in range(len(mResult[i])) :
+            result = 0
+            for k in range(len(mtrx2)) :
+                result += mtrx1[i][k] * mtrx2[k][j]
+            mResult[i][j] = result
+    return mResult
+
+def vectorToImg(v, row,col) :
+    # Mengubah vektor v menjadi grid pixel gambar berukuran row x col
+    # KAMUS LOKAL
+    # i,j,ctr : integer
+    # imgGrid, img : array of array of int
+    # gridRow : array of int
+
+    # ALGORITMA
+    img = np.asarray(v.reshape(row,col), dtype=np.uint8)
+    return img
+
+def split_test_train(inputDir, outputDir,x) :
+    # Membagi file di inputDir menjadi training dan testing dataset dan memasukkannya ke folder outputDir
+    # KAMUS LOKAL
+
+    # ALGORITMA
+    splitfolders.ratio(inputDir, output=outputDir, seed=1337, ratio = (x, 1-x))
+    return 
+
+# PROGRAM UTAMA
+# KAMUS
+# imgVectorMatrix, imgList : array of array of int
+
+# Bagi gambar menjadi training dan test dataset
+data_dir = "D:\OneDrive - Institut Teknologi Bandung\Folder Kuliah\Sem 3\Aljabar Linier dan Geometri\Tubes\Tubes 2\lfw-funneled"
+main_dir = "D:\OneDrive - Institut Teknologi Bandung\Folder Kuliah\Sem 3\Aljabar Linier dan Geometri\Tubes\Tubes 2"
+split_test_train(data_dir, "split data", 0.8)
+output_dir = main_dir+"\split data"
 # folder_dir = r"C:\Users\linal\OneDrive - Institut Teknologi Bandung\Folder Kuliah\Sem 3\Aljabar Linier dan Geometri\Tubes\Tubes 2\lfw-funneled\Angelina_Jolie"
-folder_dir = r"..\Tubes2Algeo\Angelina_Jolie"
-imgMatrix = []
+imgVectorMatrix = []
 avgVector = []
 length = 0
 
-imgList = [cv2.imread(image,0) for image in glob.glob(f'{folder_dir}/*')]
-rows,cols = imgList[0].shape
-for images in imgList :
-    ctr = 0
-    length += 1
-    
+# Wajah rata-rata dan bagi set training serta testing
+trainingImage = np.array( [np.array(cv2.imread(image,0)) for image in glob.glob(f'{output_dir}/train/*/*')])
+testImage = [cv2.imread(image,0) for image in glob.glob(f'{output_dir}/val/*/*')]
+
+rows,cols = trainingImage[0].shape
+
+for images in trainingImage :
     # List penampung nilai pixel
-    imgPixelList = []
-    
-    for i in range(rows) :
-        for j in range(cols) :
-            imgPixelList.append(images[i][j])
-            if (len(avgVector) < rows*cols) :
-                avgVector.append(int(images[i][j]))
-            else :
-                avgVector[ctr] += int(images[i][j])
-                ctr += 1
-                
-    imgMatrix.append(imgPixelList)
-    
-print(avgVector)
+    imgPixelList = images.flatten()
+    if (length == 0) :
+        avgVector = imgPixelList.astype('int')
+    else :
+        avgVector += imgPixelList          
+    imgVectorMatrix.append(imgPixelList)
+    length += 1
 
 for i in range(len(avgVector)) :
     avgVector[i] //= length
+avgVector = np.asarray(avgVector, dtype=np.uint8)
+# print(vectorToImg(avgVector,rows,cols))
+cv2.imshow("average face",vectorToImg(avgVector,rows,cols))
+cv2.waitKey(0) 
 
-print(avgVector)
+for i in range (len(imgVectorMatrix)) :
+    imgVectorMatrix[i] -= avgVector
 
-ctr = 0
-avg = []
-for i in range(rows) :
-    avgRow = []
-    for j in range (cols) :
-        avgRow.append(avgVector[ctr])
-        ctr += 1
-    avg.append(avgRow)
-    
-avgImage = np.asarray(avg, dtype=np.uint8)
-cv2.imshow("average",avgImage)
+# print(imgVectorMatrix)
+i = 1
+for imgData in imgVectorMatrix :
+    filename = "./norm face test/normFace"+str(i)+".jpg"
+    cv2.imwrite(filename,vectorToImg(imgData,rows,cols))
+    i += 1
 
-for data in imgMatrix :
-    for i in range(len(avgVector)) :
-        data[i] -= avgVector[i]
+# Matrix covariance
+# mImgTrans = transpose(imgVectorMatrix)
+# covariance = multiplyMatrix(imgVectorMatrix, mImgTrans)
+# print(len(covariance))
+# print(len(covariance[0]))
